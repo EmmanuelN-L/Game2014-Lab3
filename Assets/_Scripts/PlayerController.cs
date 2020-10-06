c4e10d7f9213157dfe1cdc9a377341dbd0377ebe
EmmanuelN-L <46698046+EmmanuelN-L@users.noreply.github.com>
2020-10-06 15:59:01 -0400

    Initial Commit

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D m_rigidBody;
+    public float horizontalBoundary;
+    public float horizontalSpeed;
+    public float maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Move()
+    {
+        float direction = 0.0f;
+        //simple touch input
+        var touch = Input.touches[0];
+        var worldTouch = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0.0f));
+
+        if (worldTouch.x > transform.position.x)
+        {
+
+        }
+
+
+        //Keyboard input
+        if(Input.GetAxis("Horizontal") >= 0.1f)
+        {
+            // direction is positive
+            direction = 1.0f;
+        }
+
+        if (Input.GetAxis("Horizontal") <= -0.1f)
+        {
+            // direction is negative
+            direction = -1.0f;
+        }
+        var newVelocity = m_rigidBody.velocity + new Vector2(direction * horizontalSpeed, 0.0f);
+
+        m_rigidBody.velocity = Vector2.ClampMagnitude(newVelocity, maxSpeed);
+        m_rigidBody.velocity *= 0.99f;
+    }
+
+    private void _CheckBounds()
+    {
+        //Check Right Bounds
+        if (transform.position.x >= horizontalBoundary)
+        {
+            transform.position = new Vector3(horizontalBoundary, transform.position.y, 0.0f);
+        }
+
+        //Check Left Bounds
+        if (transform.position.x <= -horizontalBoundary)
+        {
+            transform.position = new Vector3(-horizontalBoundary, transform.position.y, 0.0f);
+        }
+    }
+}
